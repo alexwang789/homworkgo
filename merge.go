c88cdd3f4185822d46fc1305d5efc74b6cc54240
alexwang789 <38711464+alexwang789@users.noreply.github.com>
2025-07-27 01:30:13 +0800

    Create merge.go

---
+++ merge.go
@@ -0,0 +1,27 @@
+func merge(intervals [][]int) [][]int {
+    
+    if len(intervals) <= 1 {
+        return intervals
+    }
+    
+    sort.Slice(intervals, func(i, j int) bool {
+        return intervals[i][0] < intervals[j][0]
+    })
+    
+    result := [][]int{}
+    current := intervals[0] 
+    
+    for i := 1; i < len(intervals); i++ {
+        if intervals[i][0] <= current[1] { 
+            if intervals[i][1] > current[1] {
+                current[1] = intervals[i][1]
+            }
+        } else { 
+            result = append(result, current)
+            current = intervals[i]
+        }
+    }
+    
+    result = append(result, current)
+    return result
+}
